edc798da8b78d2848a054b9aa72dc25f48a818e9
Blessing <tioluwani001@gmail.com>
2024-09-19 21:33:56 -0700

    content edits

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-function roll() {
-	// Generate random numbers
-	var diceNumber = Math.floor(Math.random() * 6 + 1);
+// Generate random numbers
+var diceNumber = Math.floor(Math.random() * 6 + 1);
 
-	var diceNumber2 = Math.floor(Math.random() * 6 + 1);
+var diceNumber2 = Math.floor(Math.random() * 6 + 1);
 
-	// get elements
-	var dice1 = document.getElementById("dice1");
-	var dice2 = document.getElementById("dice2");
+// get elements
+var dice1 = document.getElementById("dice1");
+var dice2 = document.getElementById("dice2");
 
-	// change dice image
-	dice1.src = "images/dice" + diceNumber + ".png";
+// change dice image
+dice1.src = "images/dice" + diceNumber + ".png";
 
-	dice2.src = "images/dice" + diceNumber2 + ".png";
+dice2.src = "images/dice" + diceNumber2 + ".png";
 
-	// check and display winner
-	var h1Text = document.getElementById("heading-text");
-	
-	if (diceNumber > diceNumber2) {
-		h1Text.innerText = "🚩Player 1 Wins!";
-		
-	} else if (diceNumber2 > diceNumber) {
-		h1Text.innerText = "Player 2 Wins!🚩";
-		
-	} else {
-		h1Text.innerText = "Draw!";
-	}
+// check and display winner
+var h1Text = document.getElementById("heading-text");
+
+if (diceNumber > diceNumber2) {
+	h1Text.innerText = "🚩Player 1 Wins!";
+} else if (diceNumber2 > diceNumber) {
+	h1Text.innerText = "Player 2 Wins!🚩";
+} else {
+	h1Text.innerText = "Draw!";
 }
 
 // function mode() {
