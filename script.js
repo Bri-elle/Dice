718e3229c66582990bdbb195df15e5c1747be077
Blessing <tioluwani001@gmail.com>
2024-09-19 21:19:39 -0700

    UI edits

--- script.js
+++ script.js
@@ -12,6 +12,19 @@ function roll() {
 	dice1.src = "images/dice" + diceNumber + ".png";
 
 	dice2.src = "images/dice" + diceNumber2 + ".png";
+
+	// check and display winner
+	var h1Text = document.getElementById("heading-text");
+	
+	if (diceNumber > diceNumber2) {
+		h1Text.innerText = "🚩Player 1 Wins!";
+		
+	} else if (diceNumber2 > diceNumber) {
+		h1Text.innerText = "Player 2 Wins!🚩";
+		
+	} else {
+		h1Text.innerText = "Draw!";
+	}
 }
 
 // function mode() {
@@ -26,10 +39,8 @@ function roll() {
 
 //   var playerText2 = document.getElementById("text3");
 //   playerText2.classList.toggle("dark2");
-  
+
 //   var playerText2 = document.getElementById("author");
 //   playerText2.classList.toggle("light");
 
-
 // }
-
